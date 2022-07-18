fbf676215a520865097d850048ca501d82d603ad
teethemoji <60784937+teethemoji@users.noreply.github.com>
2022-07-18 12:21:13 +0900

    [FEAT] Fork Tool 관련 Model 생성 #4

---
+++ Puhaha/Puhaha/Models/ForkTool.swift
@@ -0,0 +1,27 @@
+//
+//  ForkTool.swift
+//  Puhaha
+//
+//  Created by 김보영 on 2022/07/18.
+//
+
+import Foundation
+import UIKit
+
+enum Tool {
+    case Spoon      // 숟가락
+    case Fork       // 포크
+    case Whisk      // 거품기
+    case Spatula    // 뒤집개
+}
+
+struct ForkTool {
+    var tool: Tool
+    var color: UIColor
+}
+
+let dummyData = [
+    ForkTool(tool: Tool.Fork, color: UIColor(rgb: 0x6A94D1)),
+    ForkTool(tool: Tool.Fork, color: UIColor(rgb: 0x6A94D1)),
+    ForkTool(tool: Tool.Fork, color: UIColor(rgb: 0x6A94D1))
+]
