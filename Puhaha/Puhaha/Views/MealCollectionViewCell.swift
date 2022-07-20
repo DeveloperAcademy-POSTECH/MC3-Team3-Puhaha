0cf9152e3890786b5a462197b520fadc8f6d603b
Ji Woong Kang <ipkjw2@gmail.com>
2022-07-20 20:14:17 +0900

    [ADD] #3 메인 음식 사진에 태그 뷰 및 사용자 이미지 추가

--- Puhaha/Puhaha/Views/MealCollectionViewCell.swift
+++ Puhaha/Puhaha/Views/MealCollectionViewCell.swift
@@ -11,7 +11,11 @@ class MealCollectionViewCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        [gradation, mealImageView, userNameLabel, userIconImageView].forEach {
+        [firstTagLabel, secondTagLabel, thirdTagLabel].forEach {
+            tagStack.addArrangedSubview($0)
+        }
+        
+        [gradation, mealImageView, userNameLabel, userIconImageView, tagStack].forEach {
             addSubview($0)
         }
         
@@ -39,14 +43,45 @@ class MealCollectionViewCell: UICollectionViewCell {
     }()
     
     var userIconImageView: UIImageView = {
-        var imageView: UIImageView = UIImageView()
+        var imageView: UIImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 48, height: 48))
+        imageView.contentMode = .scaleAspectFit
+        imageView.sizeToFit()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
+    var firstTagLabel: UILabel = {
+        var label: UILabel = UILabel()
+        label.textColor = .white
+        label.layer.cornerRadius = 20
+        return label
+    }()
+    var secondTagLabel: UILabel = {
+        var label: UILabel = UILabel()
+        label.textColor = .white
+        label.layer.cornerRadius = 20
+        return label
+    }()
+    var thirdTagLabel: UILabel = {
+        var label: UILabel = UILabel()
+        label.textColor = .white
+        label.layer.cornerRadius = 20
+        return label
+    }()
+    
+    var tagStack: UIStackView = {
+        var stackView: UIStackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 6
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     var gradation: UIImageView = {
-        var imageView: UIImageView = UIImageView()
-        imageView.backgroundColor = UIColor(named: "MainColor")
+        var imageView: UIImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 209, height: 254))
+        imageView.setTranparentGradientImage(startColor: UIColor(named: "GradientStartColor")!, middleColor: UIColor(named: "GradientMiddleColor")!, endColor: UIColor(named: "tranparent")!)
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
@@ -55,6 +90,20 @@ class MealCollectionViewCell: UICollectionViewCell {
         mealImageView.image = meal.mealImage
         userNameLabel.text = meal.userName
         userIconImageView.image = meal.userIcon
+        
+        if meal.tag[0].content != "" {
+            firstTagLabel.text = "#\(meal.tag[0].content)"
+            firstTagLabel.backgroundColor = meal.tag[0].tagColor
+        }
+        
+        if meal.tag[1].content != "" {
+            secondTagLabel.text = "#\(meal.tag[1].content)"
+            secondTagLabel.backgroundColor = meal.tag[1].tagColor
+        }
+        if meal.tag[2].content != "" {
+            thirdTagLabel.text = "#\(meal.tag[2].content)"
+            thirdTagLabel.backgroundColor = meal.tag[2].tagColor
+        }
     }
     
     private func setConstraints() {
@@ -63,12 +112,36 @@ class MealCollectionViewCell: UICollectionViewCell {
         mealImageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
         mealImageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
         
-        userNameLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -96).isActive = true
-        userNameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 18).isActive = true
-        
-        gradation.topAnchor.constraint(equalTo: topAnchor, constant: 254).isActive = true
+        gradation.topAnchor.constraint(equalTo: topAnchor, constant: 221).isActive = true
         gradation.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
         gradation.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
         gradation.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        
+        userNameLabel.topAnchor.constraint(equalTo: gradation.topAnchor, constant: 18).isActive = true
+        userNameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 18).isActive = true
+
+        userIconImageView.topAnchor.constraint(equalTo: userNameLabel.topAnchor).isActive = true
+        userIconImageView.leadingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -94).isActive = true
+        userIconImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -28).isActive = true
+        userIconImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -88).isActive = true
+        
+        tagStack.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 22).isActive = true
+        tagStack.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -30).isActive = true
+    }
+}
+
+extension UIImageView {
+    func setTranparentGradientImage(startColor: UIColor, middleColor: UIColor, endColor: UIColor) {
+        let gradient: CAGradientLayer = {
+            let layout = CAGradientLayer()
+            layout.colors = [endColor.cgColor, startColor.cgColor, startColor.cgColor]
+            layout.frame = self.frame
+            layout.locations = [0.0, 0.25, 1.0]
+            layout.startPoint = CGPoint(x: 0.0, y: 0.0)
+            layout.endPoint = CGPoint(x: 0.0, y: 2.0)
+            
+            return layout
+        }()
+        self.layer.insertSublayer(gradient, at: 1)
     }
 }
