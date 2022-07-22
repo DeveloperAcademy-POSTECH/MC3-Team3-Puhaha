48b7a3b2c82f161bbebbe72f4808dcc4bff503ff
Ji Woong Kang <ipkjw2@gmail.com>
2022-07-22 20:15:02 +0900

    [CHORE] modify SampleData of Family and Tag model

--- Puhaha/Puhaha/Models/Family.swift
+++ Puhaha/Puhaha/Models/Family.swift
@@ -18,7 +18,7 @@ extension Family {
     static var sampleFamilyMemebers = [
         Family(name: "모두", userIcon: UIImage(named: "IconEveryoneFilter")!, isSelected: true),
         Family(name: "콜리", userIcon: UIImage(named: "Colli")!),
-        Family(name: "키", userIcon: UIImage(named: "Key")!, isSelected: true),
+        Family(name: "키", userIcon: UIImage(named: "Key")!),
         Family(name: "레나", userIcon: UIImage(named: "Lena")!),
         Family(name: "티모", userIcon: UIImage(named: "Teemo")!),
         Family(name: "우기", userIcon: UIImage(named: "Woogy")!)
--- Puhaha/Puhaha/Models/Tag.swift
+++ Puhaha/Puhaha/Models/Tag.swift
@@ -16,9 +16,9 @@ struct Tag {
 #if DEBUG
 extension Tag {
     static var sampleTag: [Tag] = [
-        Tag(content: "오늘 점저", tagColor: UIColor(named: "TagColorViolet")!),
-        Tag(content: "꿀맛", tagColor: UIColor(named: "TagColorPink")!),
-        Tag(content: "디저트", tagColor: UIColor(named: "TagColorPastelBlue")!)
+        Tag(content: "오늘 점저", tagColor: UIColor(named: "VioletTagBackgroundColor")!),
+        Tag(content: "꿀맛", tagColor: UIColor(named: "PinkTagBackgroundColor")!),
+        Tag(content: "디저트", tagColor: UIColor(named: "PastelBlueTagBackgroundColor")!)
     ]
 }
 #endif
