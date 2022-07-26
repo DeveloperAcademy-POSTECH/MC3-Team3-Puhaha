cc726939644edd3c3910553896c5c2f29d466944
Ji Woong Kang <ipkjw2@gmail.com>
2022-07-26 23:12:06 +0900

    [CHORE] lena, teemo 리뷰 반영
    * 콕 찌르기 버튼 동적 크기 반영
    * EmptyMealCardView.swift의 UIButton extension alignTextBelow 함수명 변경 -> alignTitleBelowImage
    *  EmptyMealCardView.swift의 UIButton extension alignTextBelow 수정 -> button title을 버튼 아래쪽으로 표시
    * StackView 이름 변경, tagStack -> mealDetailTagStackView

--- Puhaha/Puhaha/Views/EmptyMealCardView.swift
+++ Puhaha/Puhaha/Views/EmptyMealCardView.swift
@@ -21,7 +21,7 @@ class EmptyMealCardView: UIView {
         button.setImage(UIImage(named: "Spoon") ?? UIImage(), for: .normal)
         button.setTitle("콕 찌르기", for: .normal)
         button.setTitleColor(UIColor.black, for: .normal)
-        button.alignTextBelow()
+        button.alignTitleBelowImage(spacing: 16)
         
         button.setBackgroundImage(UIImage(named: "PokeButtonBackgroundImgae"), for: .normal)
         return button
@@ -46,12 +46,15 @@ class EmptyMealCardView: UIView {
         pokeButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            stateLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
             stateLabel.topAnchor.constraint(equalTo: topAnchor, constant: 72),
+            stateLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: UIScreen.main.bounds.width / 8.67),
+            stateLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -(UIScreen.main.bounds.width / 8.67)),
+            stateLabel.bottomAnchor.constraint(equalTo: topAnchor, constant: 72 + stateLabel.intrinsicContentSize.height),
             
             pokeButton.topAnchor.constraint(equalTo: stateLabel.bottomAnchor, constant: 53),
-            
-            pokeButton.centerXAnchor.constraint(equalTo: centerXAnchor)
+            pokeButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: UIScreen.main.bounds.width / 8.67),
+            pokeButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -(UIScreen.main.bounds.width / 8.67)),
+            pokeButton.bottomAnchor.constraint(equalTo: stateLabel.bottomAnchor, constant: 53 + UIScreen.main.bounds.width * 0.77)
         ])
     }
 }
@@ -63,7 +66,7 @@ extension UIButton {
      해당 함수는 ios 15부터 사용되지 않는 함수들을 사용하기 때문에 디자인이 확정되면 아래 링크를 참고하는 방식으로 바꿔야 한다.
      UIButton.Configuration apple doc 링크:  https://developer.apple.com/documentation/uikit/uibutton/configuration
      */
-    func alignTextBelow(spacing: CGFloat = 8.0) {
+    func alignTitleBelowImage(spacing: CGFloat = 8.0) {
         guard let image = self.imageView?.image else {
             return
         }
@@ -80,7 +83,7 @@ extension UIButton {
             NSAttributedString.Key.font: titleLabel.font as Any
         ])
         
-        titleEdgeInsets = UIEdgeInsets(top: spacing, left: -image.size.width, bottom: -image.size.height, right: 0)
-        imageEdgeInsets = UIEdgeInsets(top: -(titleSize.height + spacing), left: 0, bottom: 0, right: -titleSize.width)
+        titleEdgeInsets = UIEdgeInsets(top: self.intrinsicContentSize.height * 4 - spacing, left: -image.size.width, bottom: titleSize.height / 2, right: 0)
+        imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: spacing, right: -titleSize.width)
     }
 }
