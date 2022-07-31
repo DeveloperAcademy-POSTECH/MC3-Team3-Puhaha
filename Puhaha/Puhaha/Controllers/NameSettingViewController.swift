bf8f1572d7b8eb705bfcd1554fb44dbd4f8a1ed9
Colli <happythgus@khu.ac.kr>
2022-07-31 15:31:48 +0900

    [CHORE] NameSettingViewController hi-fi design 구현 완료

--- Puhaha/Puhaha/Controllers/NameSettingViewController.swift
+++ Puhaha/Puhaha/Controllers/NameSettingViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 class NameSettingViewController: UIViewController {
     
     private let guidingTextLabel: UILabel = {
-      let label = UILabel()
-      label.text = "가족에게 보여질 이름을 입력해주세요"
-      label.textColor = .black
-      label.font = .systemFont(ofSize: 20, weight: .semibold)
-      return label
+        let label = UILabel()
+        label.text = "가족에게 보여질 이름을 입력해주세요"
+        label.textColor = .black
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        return label
     }()
     
     private var nameTextField: UITextField = {
@@ -24,65 +25,54 @@ class NameSettingViewController: UIViewController {
         return textField
     }()
     
-    private let nextButton: CustomedLoginButton = {
-         let button = CustomedLoginButton()
-         button.setTitle("나만의 도구 제작하기", for: .normal)
-         button.setTitleColor(UIColor.black, for: .normal)
+    private let nextButton: CustomedButton = {
+        let button = CustomedButton()
+        button.setTitle("나만의 도구 제작하기", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
         button.backgroundColor = UIColor.customYellow
-
-        button.addTarget(NameSettingViewController.self,
-                          action: #selector(nextButtonTapped),
-                          for: .touchUpInside)
-         return button
-     }()
+        
+        button.addTarget(self,
+                         action: #selector(nextButtonTapped),
+                         for: .touchUpInside)
+        return button
+    }()
     
     @objc private func nextButtonTapped() {
-        
+        let pokeToolCustomizingViewController = PokeToolCustomizingViewController()
+        self.navigationController?.pushViewController(pokeToolCustomizingViewController, animated: true)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
         navigationController?.setNavigationBarHidden(true, animated: false)
-
+        nameTextField.becomeFirstResponder()
+        
         [guidingTextLabel, nameTextField, nextButton].forEach {
             view.addSubview($0)
         }
-        
         configureConstraints()
     }
     
     override func viewDidLayoutSubviews() {
-         super.viewDidLayoutSubviews()
-         nameTextField.setUnderLine()
-     }
+        super.viewDidLayoutSubviews()
+        nameTextField.setUnderLine()
+    }
     
     private func configureConstraints() {
         guidingTextLabel.translatesAutoresizingMaskIntoConstraints = false
         nameTextField.translatesAutoresizingMaskIntoConstraints = false
         nextButton.translatesAutoresizingMaskIntoConstraints = false
-
+        
         guidingTextLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        guidingTextLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: view.bounds.height * 0.3).isActive = true
-
-        nameTextField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.65).isActive = true
+        guidingTextLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: view.bounds.height * 0.2).isActive = true
+        guidingTextLabel.widthAnchor.constraint(equalTo: nextButton.widthAnchor).isActive = true
+        
+        nameTextField.widthAnchor.constraint(equalTo: nextButton.widthAnchor).isActive = true
         nameTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        nameTextField.topAnchor.constraint(equalTo: guidingTextLabel.bottomAnchor, constant: view.bounds.height * 0.02).isActive = true
-
+        nameTextField.topAnchor.constraint(equalTo: guidingTextLabel.bottomAnchor, constant: view.bounds.height * 0.04).isActive = true
+        
         nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        nextButton.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: view.bounds.height * 0.15).isActive = true
+        nextButton.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: view.bounds.height * 0.55).isActive = true
     }
 }
-
-extension UITextField {
-     func setUnderLine() {
-         let bottomLine = CALayer()
-         bottomLine.frame = CGRect(x: 0.0,
-                                   y: self.bounds.height + 3,
-                                   width: self.bounds.width,
-                                   height: 1.5)
-         bottomLine.backgroundColor = UIColor.lightGray.cgColor
-         self.borderStyle = UITextField.BorderStyle.none
-         self.layer.addSublayer(bottomLine)
-     }
- }
