69655c5945c5869b907aa5375d050c3877751f2b
Ji Woong Kang <ipkjw2@gmail.com>
2022-08-04 19:43:55 +0900

    [CHORE] change dayText format

--- Puhaha/Puhaha/Global/Extensioon/Date+Today.swift
+++ Puhaha/Puhaha/Global/Extensioon/Date+Today.swift
@@ -9,10 +9,14 @@ import Foundation
 
 extension Date {
     var dayText: String {
-        let weekText = formatted(.dateTime.weekday(.wide))
-        let dateText = formatted(.dateTime.month(.abbreviated).day())
-        let dateAndTimeFormat = NSLocalizedString("%@ %@", comment: "Date and Weekday format string")
-        return String(format: dateAndTimeFormat, dateText, weekText)
+//        let weekText = formatted(.dateTime.weekday(.wide))
+//        let dateText = formatted(.dateTime.month(.abbreviated).day())
+//        let dateAndTimeFormat = NSLocalizedString("%@ %@", comment: "Date and Weekday format string")
+//        return String(format: dateAndTimeFormat, dateText, weekText)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "M월 d일 EEEE"
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        return dateFormatter.string(from: self)
     }
     
     var timeText: String {
