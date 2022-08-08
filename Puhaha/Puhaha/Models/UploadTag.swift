3af01c0b463dd5abe57d3b55be08db2bd99da08b
lena <lenaminn@gmail.com>
2022-08-08 18:25:25 +0900

    [CHORE] 불필요한 인스턴스 삭제

--- Puhaha/Puhaha/Models/UploadTag.swift
+++ Puhaha/Puhaha/Models/UploadTag.swift
@@ -8,8 +8,7 @@
 import Foundation
 
 struct UploadTag {
-    var tagTitleName: String
-    var tagContents: [Tag] = [Tag]()
+    var tagContents = [Tag]()
 }
 
 struct UploadInformation {
@@ -20,27 +19,22 @@ struct UploadInformation {
     var uploadedTime: String
 }
 
-#if DEBUG
 extension UploadTag {
     static let uploadTags = [
-        UploadTag(tagTitleName: "시간",
-                  tagContents: [Tag(content: "아침", backgroundColor: .customPurple),
+        UploadTag(tagContents: [Tag(content: "아침", backgroundColor: .customPurple),
                                 Tag(content: "아점", backgroundColor: .customPurple),
                                 Tag(content: "점심", backgroundColor: .customPurple),
                                 Tag(content: "점저", backgroundColor: .customPurple),
                                 Tag(content: "저녁", backgroundColor: .customPurple),
                                 Tag(content: "야식", backgroundColor: .customPurple)]),
-        UploadTag(tagTitleName: "메뉴",
-                  tagContents: [Tag(content: "한식", backgroundColor: .customBlue),
+        UploadTag(tagContents: [Tag(content: "한식", backgroundColor: .customBlue),
                                 Tag(content: "중식", backgroundColor: .customBlue),
                                 Tag(content: "일식", backgroundColor: .customBlue),
                                 Tag(content: "디저트", backgroundColor: .customBlue),
                                 Tag(content: "양식", backgroundColor: .customBlue)]),
-        UploadTag(tagTitleName: "기분",
-                  tagContents: [Tag(content: "꿀맛", backgroundColor: .customGreen),
+        UploadTag(tagContents: [Tag(content: "꿀맛", backgroundColor: .customGreen),
                                 Tag(content: "최고에요", backgroundColor: .customGreen),
                                 Tag(content: "별로에요", backgroundColor: .customGreen),
                                 Tag(content: "맛없엉", backgroundColor: .customGreen)])
     ]
 }
-#endif
