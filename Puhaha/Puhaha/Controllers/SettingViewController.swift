df71e73f773e1a4926bc9db33f252d6c7237edd2
Ji Woong Kang <ipkjw2@gmail.com>
2022-08-25 20:36:45 +0900

    [CHORE] 중복 코드 제거

--- Puhaha/Puhaha/Controllers/SettingViewController.swift
+++ Puhaha/Puhaha/Controllers/SettingViewController.swift
@@ -46,7 +46,6 @@ class SettingViewController: UIViewController {
         super.viewDidLoad()
         navigationItem.backBarButtonItem?.title = ""
         navigationController?.navigationBar.tintColor = .tintColor
-        navigationItem.title = "설정"
         view.backgroundColor = UIColor.white
         navigationController?.navigationBar.backgroundColor = .clear
         
